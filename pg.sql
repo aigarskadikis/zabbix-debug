08fd7ccb5f2dae44bf401a7c4a041e118d66d9d3
catonrug <aigars.kadikis@gmail.com>
2020-02-17 13:25:57 +0200

    n

--- pg.sql
+++ pg.sql
@@ -1,5 +1,25 @@
 
 
+/* live variables and database size */
+\o postgresql-9-live-variables.txt
+
+SELECT nspname || '.' || relname AS "relation",
+    pg_size_pretty(pg_total_relation_size(C.oid)) AS "total_size"
+  FROM pg_class C
+  LEFT JOIN pg_namespace N ON (N.oid = C.relnamespace)
+  WHERE nspname NOT IN ('pg_catalog', 'information_schema')
+    AND C.relkind <> 'i'
+    AND nspname !~ '^pg_toast'
+  ORDER BY pg_total_relation_size(C.oid) DESC;
+
+SHOW ALL;
+
+\o
+
+\q
+
+
+
 
 /* authorize in pg database */ 
 su - postgres -c 'psql zabbix'
