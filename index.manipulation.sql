462301a901c786debfddd13fb6cd173c425d846c
catonrug <aigars.kadikis@gmail.com>
2020-04-28 15:01:05 +0300

    next

--- index.manipulation.sql
+++ index.manipulation.sql
@@ -17,6 +17,71 @@ CREATE TABLE `history` (
 insert ignore into aste select * from history where clock> UNIX_TIMESTAMP(now()-INTERVAL 1 hour);
 
 
+[mysqld]
+group_concat_max_len = 1000000
+
+SET SESSION group_concat_max_len = 1000000;
+
+
+/* float values */
+SELECT DISTINCT hosts.host,
+                GROUP_CONCAT(items.itemid) AS 'float'
+FROM items
+JOIN hosts ON (hosts.hostid=items.hostid)
+WHERE items.value_type=0
+  AND hosts.status IN (0,1)
+GROUP BY items.hostid
+\G
+
+/* integers */
+SELECT DISTINCT hosts.host,
+                GROUP_CONCAT(items.itemid) AS 'integers'
+FROM items
+JOIN hosts ON (hosts.hostid=items.hostid)
+WHERE items.value_type=1
+  AND hosts.status IN (0,1)
+GROUP BY items.hostid
+\G
+
+/* character */
+SELECT DISTINCT hosts.host,
+                GROUP_CONCAT(items.itemid) AS 'character'
+FROM items
+JOIN hosts ON (hosts.hostid=items.hostid)
+WHERE items.value_type=2
+  AND hosts.status IN (0,1)
+GROUP BY items.hostid
+\G
+
+
+
+SELECT DISTINCT hosts.hostid,
+                GROUP_CONCAT(items.itemid) AS 'integers'
+FROM items
+JOIN hosts ON (hosts.hostid=items.hostid)
+WHERE items.value_type=1
+  AND hosts.status IN (0,1)
+GROUP BY items.hostid
+\G
+
+
+
+
+SELECT DISTINCT hosts.hostid AS "PARTITION",GROUP_CONCAT(items.itemid) AS "VALUES IN (" FROM items JOIN hosts ON (hosts.hostid=items.hostid) WHERE items.value_type=1 AND hosts.status IN (0,1) GROUP BY items.hostid\G
+
+
+mysql zabbix -sN -e 'SELECT DISTINCT hosts.hostid AS "PARTITION",GROUP_CONCAT(items.itemid) AS "VALUES IN (" FROM items JOIN hosts ON (hosts.hostid=items.hostid) WHERE items.value_type=1 AND hosts.status IN (0,1) GROUP BY items.hostid\G'
+
+
+mysql zabbix -sN -e 'SELECT DISTINCT hosts.hostid,GROUP_CONCAT(items.itemid) FROM items JOIN hosts ON (hosts.hostid=items.hostid) WHERE items.value_type=1 AND hosts.status IN (0,1) GROUP BY items.hostid;' | \
+sed 's|^|PARTITION hostid_|;s|\t| VALUES IN (|;s|$|),|'
+
+tr '\t' ' VALUES IN ('
+
+sed "s|^|PARTITION hostid|"
+sed 's/\t/,/g'
+
+
 DROP INDEX history_1 ON aste;
 
 
